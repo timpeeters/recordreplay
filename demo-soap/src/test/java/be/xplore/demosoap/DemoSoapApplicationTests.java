e16e847ffdc03bae750706faff0ef86860e3c4f2
Stijn Schack <stijn.schack@gmail.com>
2019-07-02 09:59:45 +0200

    #10 fixed imports and indentations.

--- demo-soap/src/test/java/be/xplore/demosoap/DemoSoapApplicationTests.java
+++ demo-soap/src/test/java/be/xplore/demosoap/DemoSoapApplicationTests.java
@@ -1,6 +1,5 @@
 package be.xplore.demosoap;
 
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,8 +8,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoSoapApplicationTests {
-
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 }
