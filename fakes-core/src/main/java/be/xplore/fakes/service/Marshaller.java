7c602a236b187608ce471df82688cda2ca8f37be
Jeroen Timmers <jeroen.timmers@icloud.com>
2019-07-04 17:00:44 +0200

    "Written/passed unit tests for JsonMarshaller"

--- fakes-core/src/main/java/be/xplore/fakes/service/Marshaller.java
+++ fakes-core/src/main/java/be/xplore/fakes/service/Marshaller.java
@@ -2,11 +2,12 @@ package be.xplore.fakes.service;
 
 import be.xplore.fakes.model.Stub;
 
+import java.io.IOException;
 import java.io.Reader;
 import java.io.Writer;
 
 public interface Marshaller {
-    void marshal(Stub stub, Writer writer);
+    void marshal(Stub stub, Writer writer) throws IOException;
 
-    Stub unMarshal(Reader reader);
+    Stub unMarshal(Reader reader) throws IOException;
 }
