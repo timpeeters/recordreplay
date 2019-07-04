4d19bff73cb8db66f86b09cc52c587c951bb03d1
Tim Peeters <timpeeters@users.noreply.github.com>
2019-07-04 09:00:44 +0200

    Update Gradle to version 5.5. (#27)
    * Update Gradle to version 5.5.
    
    * PMD & Checkstyle config.

--- demorest/src/main/java/be/xplore/demorest/service/exceptions/UserValidationException.java
+++ demorest/src/main/java/be/xplore/demorest/service/exceptions/UserValidationException.java
@@ -1,6 +1,7 @@
 package be.xplore.demorest.service.exceptions;
 
 public class UserValidationException extends Exception {
+    private static final long serialVersionUID = 1L;
 
     public UserValidationException(String message) {
         super(message);
