1953fbe1c24c30735e778d2c804115277ea78cc2
Tim Peeters <tim.peeters@me.com>
2019-07-04 09:56:24 +0200

    Implement soap demo service.

--- demo-soap/src/main/java/be/xplore/demosoap/DemoSoapApplication.java
+++ demo-soap/src/main/java/be/xplore/demosoap/DemoSoapApplication.java
@@ -1,32 +1,11 @@
 package be.xplore.demosoap;
 
-import org.apache.cxf.Bus;
-import org.apache.cxf.bus.spring.SpringBus;
-import org.apache.cxf.transport.servlet.CXFServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.web.servlet.DispatcherServletPath;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DemoSoapApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoSoapApplication.class, args);
-    }
-
-    @Bean
-    public ServletRegistrationBean<CXFServlet> dispatcherServlet() {
-        return new ServletRegistrationBean<>(new CXFServlet());
-    }
-
-    @Bean
-    public DispatcherServletPath dispatcherServletPath() {
-        return () -> "/soap-api/*";
-    }
-
-    @Bean(name = Bus.DEFAULT_BUS_ID)
-    public SpringBus springBus() {
-        return new SpringBus();
     }
 }
