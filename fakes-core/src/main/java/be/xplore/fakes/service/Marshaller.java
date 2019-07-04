048ec62638069052ccf08cda249b338cb57bbf95
Stijn Schack <stijn.schack@gmail.com>
2019-07-04 11:22:01 +0200

    #12 Created Marshaller and Repository interfaces

---
+++ fakes-core/src/main/java/be/xplore/fakes/service/Marshaller.java
@@ -0,0 +1,12 @@
+package be.xplore.fakes.service;
+
+import be.xplore.fakes.model.Stub;
+
+import java.io.Reader;
+import java.io.Writer;
+
+public interface Marshaller {
+    void marshal(Stub stub, Writer writer);
+
+    Stub unMarshal(Reader reader);
+}
