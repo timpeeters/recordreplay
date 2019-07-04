187a6424052878b67a141733808dafb23d427f71
Schackman <stijn.schack@gmail.com>
2019-07-04 17:42:43 +0200

    Revert "add and list Stubs in MemoryRepository (#36)"
    This reverts commit 5dbeb1711eb7d85f3cff4f3b310a7e96001ab8e3.

--- fakes-core/src/main/java/be/xplore/fakes/service/Repository.java
+++ fakes-core/src/main/java/be/xplore/fakes/service/Repository.java
@@ -1,12 +1,11 @@
 package be.xplore.fakes.service;
 
 import be.xplore.fakes.model.Stub;
-import be.xplore.fakes.service.except.InvalidStubException;
 
 import java.util.List;
 
 public interface Repository {
-    void add(Stub stub) throws InvalidStubException;
+    void add(Stub stub);
 
     List<Stub> find();
 }
