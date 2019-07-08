0ea729fd65487733b5a7c06b79503a420880a562
Stijn Schack <stijn.schack@gmail.com>
2019-07-08 11:52:22 +0200

    improved code coverage

--- fakes-core/src/test/java/be/xplore/fakes/model/RequestTest.java
+++ fakes-core/src/test/java/be/xplore/fakes/model/RequestTest.java
@@ -3,20 +3,48 @@ package be.xplore.fakes.model;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 public class RequestTest {
 
     @Test
-    public void checkEquality(){
-        Request request1 = new Request()
-                .setPath("/test")
-                .setMethod(RequestMethod.GET);
-
-        Request request2 = new Request()
-                .setPath("/test")
-                .setMethod(RequestMethod.GET);
-
-        assertEquals("Equals method not implemented correctly",request1, request2);
+    public void checkEquality() {
+        Request request1 = getNewExampleRequest();
+        Request request2 = getNewExampleRequest();
+        assertEquals(request1, request1);
+        assertEquals("Equals method not implemented correctly", request1, request2);
     }
 
+    @Test
+    public void checkNotEqual() {
+        Request request1 = getNewExampleRequest();
+        Request request2 = new Request()
+                .setMethod(RequestMethod.POST);
+        assertNotEquals(request1, request2);
+    }
+
+    @SuppressWarnings("ConstantConditions")
+    @Test
+    public void checkNullAndOtherClass() {
+        Request request1 = getNewExampleRequest();
+        Request request2 = null;
+
+        assertNotEquals(request1, request2);
+        assertNotEquals(request1, 5);
+    }
+
+    @Test
+    public void hashCodeTest() {
+        assertEquals(getNewExampleRequest().hashCode(), getNewExampleRequest().hashCode());
+        assertNotEquals(getNewExampleRequest().hashCode(),
+                getNewExampleRequest().setMethod(RequestMethod.POST).hashCode());
+        assertNotEquals(getNewExampleRequest().hashCode(), getNewExampleRequest().setPath("/abc").hashCode());
+    }
+
+
+    private Request getNewExampleRequest() {
+        return new Request()
+                .setPath("/test")
+                .setMethod(RequestMethod.GET);
+    }
 }
--- fakes-core/src/test/java/be/xplore/fakes/model/ResponseTest.java
+++ fakes-core/src/test/java/be/xplore/fakes/model/ResponseTest.java
@@ -3,21 +3,49 @@ package be.xplore.fakes.model;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 public class ResponseTest {
 
     @Test
-    public void checkEquality(){
-        Response response1 = new Response()
-                .setStatusCode(200)
-                .setStatusText("OK");
-
-        Response response2 = new Response()
-                .setStatusCode(200)
-                .setStatusText("OK");
-
+    public void checkEquality() {
+        Response response1 = getNewExampleResponse();
+        Response response2 = getNewExampleResponse();
+        assertEquals(response1, response1);
         assertEquals("Equals method not implemented correctly", response1, response2);
+    }
 
+    @Test
+    public void checkNotEqual() {
+        Response response1 = getNewExampleResponse();
+        Response response2 = new Response()
+                .setStatusCode(404)
+                .setStatusText("NOT FOUND");
+
+        assertNotEquals(response1, response2);
+    }
+
+    @SuppressWarnings("ConstantConditions")
+    @Test
+    public void checkNullAndOtherClass() {
+        Response response1 = getNewExampleResponse();
+        Response response2 = null;
+
+        assertNotEquals(response1, response2);
+        assertNotEquals(response1, 5);
+    }
+
+    @Test
+    public void hashCodeTest() {
+        assertEquals(getNewExampleResponse().hashCode(), getNewExampleResponse().hashCode());
+        assertNotEquals(getNewExampleResponse().hashCode(), getNewExampleResponse().setStatusCode(404).hashCode());
+        assertNotEquals(getNewExampleResponse().hashCode(), getNewExampleResponse().setStatusText("abc").hashCode());
+    }
+
+    private Response getNewExampleResponse() {
+        return new Response()
+                .setStatusCode(200)
+                .setStatusText("OK");
     }
 
 }
