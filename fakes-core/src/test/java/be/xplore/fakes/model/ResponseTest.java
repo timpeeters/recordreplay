e0e012e1d768da75651c88064858bb69b4e60b8f
Stijn Schack <stijn.schack@gmail.com>
2019-07-08 14:35:12 +0200

    improved coverage

--- fakes-core/src/test/java/be/xplore/fakes/model/ResponseTest.java
+++ fakes-core/src/test/java/be/xplore/fakes/model/ResponseTest.java
@@ -9,43 +9,43 @@ public class ResponseTest {
 
     @Test
     public void checkEquality() {
-        Response response1 = getNewExampleResponse();
-        Response response2 = getNewExampleResponse();
-        assertEquals(response1, response1);
-        assertEquals("Equals method not implemented correctly", response1, response2);
+        Response response = getNewExampleResponse1();
+        assertEquals(response, response);
+        assertEquals("Equals method not implemented correctly", response, getNewExampleResponse1());
     }
 
     @Test
     public void checkNotEqual() {
-        Response response1 = getNewExampleResponse();
-        Response response2 = new Response()
-                .setStatusCode(404)
-                .setStatusText("NOT FOUND");
-
-        assertNotEquals(response1, response2);
+        assertNotEquals(getNewExampleResponse1(), getNewExampleResponse2());
+        assertNotEquals(getNewExampleResponse1(),
+                getNewExampleResponse1().setStatusText(getNewExampleResponse2().getStatusText()));
     }
 
     @SuppressWarnings("ConstantConditions")
     @Test
     public void checkNullAndOtherClass() {
-        Response response1 = getNewExampleResponse();
-        Response response2 = null;
-
-        assertNotEquals(response1, response2);
-        assertNotEquals(response1, 5);
+        Response response = getNewExampleResponse1();
+        Response nullResponse = null;
+        assertNotEquals(response, nullResponse);
+        assertNotEquals(response, 5);
     }
 
     @Test
     public void hashCodeTest() {
-        assertEquals(getNewExampleResponse().hashCode(), getNewExampleResponse().hashCode());
-        assertNotEquals(getNewExampleResponse().hashCode(), getNewExampleResponse().setStatusCode(404).hashCode());
-        assertNotEquals(getNewExampleResponse().hashCode(), getNewExampleResponse().setStatusText("abc").hashCode());
+        assertEquals(getNewExampleResponse1().hashCode(), getNewExampleResponse1().hashCode());
+        assertNotEquals(getNewExampleResponse1().hashCode(), getNewExampleResponse2().hashCode());
     }
 
-    private Response getNewExampleResponse() {
+    private Response getNewExampleResponse1() {
         return new Response()
                 .setStatusCode(200)
                 .setStatusText("OK");
+    }
+
+    private Response getNewExampleResponse2() {
+        return new Response()
+                .setStatusCode(404)
+                .setStatusText("NOT FOUND");
     }
 
 }
