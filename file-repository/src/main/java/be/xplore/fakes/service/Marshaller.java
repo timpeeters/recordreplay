34278ff42cc481dc60910e48bc79d51677f77186
Jeroen Timmers <jeroen.timmers@icloud.com>
2019-07-08 11:10:08 +0200

    "Refactored unmarshal to return Stub-list using iterator"

--- file-repository/src/main/java/be/xplore/fakes/service/Marshaller.java
+++ file-repository/src/main/java/be/xplore/fakes/service/Marshaller.java
@@ -5,9 +5,11 @@ import be.xplore.fakes.model.Stub;
 import java.io.IOException;
 import java.io.Reader;
 import java.io.Writer;
+import java.util.List;
 
 public interface Marshaller {
     void marshal(Stub stub, Writer writer) throws IOException;
 
-    Stub unMarshal(Reader reader) throws IOException;
+    List<Stub> unMarshal(Reader reader) throws IOException;
+
 }
--- json-marshaller/src/main/java/be/xplore/jsonmarshaller/JsonMarshaller.java
+++ json-marshaller/src/main/java/be/xplore/jsonmarshaller/JsonMarshaller.java
@@ -2,12 +2,16 @@ package be.xplore.jsonmarshaller;
 
 import be.xplore.fakes.model.Stub;
 import be.xplore.fakes.service.Marshaller;
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.databind.MappingIterator;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 
 import java.io.IOException;
 import java.io.Reader;
 import java.io.Writer;
+import java.util.ArrayList;
+import java.util.List;
 
 public class JsonMarshaller implements Marshaller {
 
@@ -17,12 +21,16 @@ public class JsonMarshaller implements Marshaller {
     public void marshal(Stub stub, Writer writer) throws IOException {
         String jsonString = mapper.writeValueAsString(stub);
         writer.append(jsonString);
-        writer.close();
     }
 
     @Override
-    public Stub unMarshal(Reader reader) throws IOException {
-        return mapper.readValue(reader, Stub.class);
+    public List<Stub> unMarshal(Reader reader) throws IOException {
+        List<Stub> stubs = new ArrayList<>();
+        MappingIterator<Stub> stubIterator = mapper.readValues(new JsonFactory().createParser(reader), Stub.class);
+        while (stubIterator.hasNext()) {
+            stubs.add(stubIterator.next());
+        }
+        return stubs;
     }
 
 }
