7a85bf091d0c778611997a225df3fcbf8b98752c
Timmersch <Jeroen.Timmers@icloud.com>
2019-07-09 15:26:54 +0200

    "Created RequestMatcher-interface and Result-class" (#66)

---
+++ fakes-core/src/main/java/be/xplore/fakes/model/Result.java
@@ -0,0 +1,15 @@
+package be.xplore.fakes.model;
+
+public class Result {
+
+    private double distance;
+
+    public double getDistance() {
+        return distance;
+    }
+
+    public Result setDistance(double distance) {
+        this.distance = distance;
+        return this;
+    }
+}
