04a1a7e402782ed56227a98e233771a67a1c1448
Stijn Schack <stijn.schack@gmail.com>
2019-07-15 17:04:01 +0200

    add serialVersionUID to DefaultHttpServlet
    issue #80

--- recordreplay-servlet/src/main/java/be/xplore/recordreplay/service/DefaultHttpServlet.java
+++ recordreplay-servlet/src/main/java/be/xplore/recordreplay/service/DefaultHttpServlet.java
@@ -7,6 +7,8 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class DefaultHttpServlet extends HttpServlet {
+    private static final long serialVersionUID = 7825358410575851102L;
+
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         super.service(req, resp);
