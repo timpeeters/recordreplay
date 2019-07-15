3c592008396d018ed007018e1771416e32020b36
Jeroen Timmers <jeroen.timmers@icloud.com>
2019-07-15 12:00:49 +0200

    "Increased code coverage of matchers"

--- fakes-core/src/test/java/be/xplore/fakes/service/RequestHeaderMatcherTests.java
+++ fakes-core/src/test/java/be/xplore/fakes/service/RequestHeaderMatcherTests.java
@@ -16,12 +16,32 @@ public class RequestHeaderMatcherTests {
 
     private final Headers sameHeaders1 = Headers.builder().headerMap(Map.of(KEY, List.of("Test1", "Test2"))).build();
     private final Headers sameHeaders2 = Headers.builder().headerMap(Map.of(KEY, List.of("Test1", "Test2"))).build();
-    private final Headers diffHeaders = Headers.builder().headerMap(Map.of(KEY, List.of("Test3", "Test4"))).build();
+    private final Headers diffHeaders =
+            Headers.builder().headerMap(Map.of(KEY, List.of("Test3", "Test4", "Test5"))).build();
     private final Headers halfHeaders = Headers.builder().headerMap(Map.of(KEY, List.of("Test1", "Test8"))).build();
     private final Request sameRequest1 = Request.Builder.get("").headers(sameHeaders1).build();
     private final Request sameRequest2 = Request.Builder.get("").headers(sameHeaders2).build();
     private final Request diffRequest = Request.Builder.get("").headers(diffHeaders).build();
     private final Request halfRequest = Request.Builder.get("").headers(halfHeaders).build();
+
+    @Test
+    public void requestParamMatcherShouldReturnZeroOnTwoEmptyHeaders() {
+        assertThat(matcher.matches(Request.Builder.post("").headers(Headers.EMPTY).build(),
+                Request.Builder.post("").headers(Headers.EMPTY).build()).getDistance())
+                .as("Correct result distance not calculated on two empty params")
+                .isEqualTo(0);
+    }
+
+    @Test
+    public void requestParamMatcherShouldReturnOneOnEmptyHeaders() {
+        assertThat(matcher.matches(Request.Builder
+                .post("")
+                .headers(Headers.EMPTY)
+                .build(), sameRequest1).getDistance())
+                .as("Correct result distance not calculated on single empty param\"")
+                .isEqualTo(1);
+    }
+
 
     @Test
     public void requestHeaderMatcherShouldReturnResultZeroOnCompleteMatch() {
@@ -43,4 +63,12 @@ public class RequestHeaderMatcherTests {
                 .as("Correct result distance not calculated")
                 .isEqualTo(0.5);
     }
+
+    @Test
+    public void sameResultOnReversedParameters() {
+        assertThat(matcher.matches(diffRequest, sameRequest1).getDistance())
+                .as("Correct result distance not calculated on reversed parameters")
+                .isEqualTo(1);
+    }
+
 }
--- fakes-core/src/test/java/be/xplore/fakes/service/RequestParamMatcherTests.java
+++ fakes-core/src/test/java/be/xplore/fakes/service/RequestParamMatcherTests.java
@@ -20,7 +20,7 @@ public class RequestParamMatcherTests {
     private final QueryParams sameQueryParams2 =
             QueryParams.builder().params(Map.of(KEY, List.of("Test1", "Test2"))).build();
     private final QueryParams diffQueryParams =
-            QueryParams.builder().params(Map.of(KEY, List.of("Test3", "Test4"))).build();
+            QueryParams.builder().params(Map.of(KEY, List.of("Test3", "Test4", "Test5"))).build();
     private final QueryParams halfQueryParams =
             QueryParams.builder().params(Map.of(KEY, List.of("Test1", "Test8"))).build();
 
@@ -29,26 +29,51 @@ public class RequestParamMatcherTests {
     private final Request diffRequest = Request.Builder.post("").queryParams(diffQueryParams).build();
     private final Request halfRequest = Request.Builder.post("").queryParams(halfQueryParams).build();
 
-
     @Test
-    public void requestParamMatcherShouldReturnResultZeroOnCompleteMatch() {
-        assertThat(matcher.matches(sameRequest1, sameRequest2).getDistance())
-                .as("Correct result distance not calculated")
+    public void requestParamMatcherShouldReturnZeroOnTwoEmptyParams() {
+        assertThat(matcher.matches(Request.Builder.post("").queryParams(QueryParams.EMPTY).build(),
+                Request.Builder.post("").queryParams(QueryParams.EMPTY).build()).getDistance())
+                .as("Correct result distance not calculated on two empty params")
                 .isEqualTo(0);
     }
 
     @Test
+    public void requestParamMatcherShouldReturnOneOnEmptyParam() {
+        assertThat(matcher.matches(Request.Builder
+                .post("")
+                .queryParams(QueryParams.EMPTY)
+                .build(), sameRequest1).getDistance())
+                .as("Correct result distance not calculated on single empty param\"")
+                .isEqualTo(1);
+    }
+
+    @Test
+    public void requestParamMatcherShouldReturnResultZeroOnCompleteMatch() {
+        assertThat(matcher.matches(sameRequest1, sameRequest2).getDistance())
+                .as("Correct result distance not calculated on match")
+                .isEqualTo(0);
+    }
+
+
+    @Test
     public void requestParamMatcherShouldReturnResultOneOnCompleteMismatch() {
         assertThat(matcher.matches(sameRequest1, diffRequest).getDistance())
-                .as("Correct result distance not calculated")
+                .as("Correct result distance not calculated on mismatch")
                 .isEqualTo(1);
     }
 
     @Test
     public void requestParamMatcherShouldReturnResultHalfOnHalfMatch() {
         assertThat(matcher.matches(sameRequest1, halfRequest).getDistance())
-                .as("Correct result distance not calculated")
+                .as("Correct result distance not calculated on half match")
                 .isEqualTo(0.5);
+    }
+
+    @Test
+    public void sameResultOnReversedParameters() {
+        assertThat(matcher.matches(diffRequest, sameRequest1).getDistance())
+                .as("Correct result distance not calculated on reversed parameters")
+                .isEqualTo(1);
     }
 
 }
