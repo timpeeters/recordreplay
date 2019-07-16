27b623d664f5b5d14b705f401315269d5b56a42c
Stijn Schack <stijn.schack@gmail.com>
2019-07-16 14:35:00 +0200

    Implemented service method Issue #80

--- recordreplay-servlet/src/main/java/be/xplore/recordreplay/service/DefaultHttpServlet.java
+++ recordreplay-servlet/src/main/java/be/xplore/recordreplay/service/DefaultHttpServlet.java
@@ -1,16 +1,32 @@
 package be.xplore.recordreplay.service;
 
-import javax.servlet.ServletException;
+import be.xplore.fakes.model.Request;
+import be.xplore.fakes.model.Response;
+import be.xplore.fakes.service.DefaultHttpClient;
+import be.xplore.fakes.service.HttpClient;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 public class DefaultHttpServlet extends HttpServlet {
     private static final long serialVersionUID = 7825358410575851102L;
 
+    private final HttpServletRequestMapper requestMapper;
+    private final ResponseMapper<HttpServletResponse> responseMapper;
+    private final HttpClient client;
+
+    public DefaultHttpServlet(ResponseMapper<HttpServletResponse> responseMapper) {
+        super();
+        requestMapper = new HttpServletRequestMapper();
+        this.responseMapper = responseMapper;
+        client = new DefaultHttpClient();
+    }
+
     @Override
-    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.service(req, resp);
+    protected void service(HttpServletRequest req, HttpServletResponse resp) {
+        Request request = requestMapper.map(req);
+        Response response = client.execute(request);
+        responseMapper.map(response, resp);
     }
 }
