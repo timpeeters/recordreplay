0acf1b88189bc1b2873b3793d0bfa56faacd9c21
Jeroen Timmers <jeroen.timmers@icloud.com>
2019-07-17 16:17:38 +0200

    "added record-usecase"

---
+++ fakes-core/src/main/java/be/xplore/fakes/usecase/RecordUseCase.java
@@ -0,0 +1,21 @@
+package be.xplore.fakes.usecase;
+
+import be.xplore.fakes.model.Request;
+import be.xplore.fakes.model.Response;
+import be.xplore.fakes.model.Stub;
+import be.xplore.fakes.service.Repository;
+
+public class RecordUseCase {
+
+    private final Repository repository;
+
+    public RecordUseCase(Repository repository) {
+        this.repository = repository;
+    }
+
+    public void record(Request request, Response response) {
+        Stub recordedStub = new Stub(request,response);
+        repository.add(recordedStub);
+    }
+
+}
