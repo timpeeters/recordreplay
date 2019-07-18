32b81a6c1d415100173d60298d9676cfe79d07d3
Timmersch <Jeroen.Timmers@icloud.com>
2019-07-18 15:05:02 +0200

    Code coverage (#107)
    * "Increased code coverage of matchers"
    
    * "Increased code coverage of Marshallers"
    
    * "Added unit tests for User-model"

---
+++ demo-soap/src/test/java/be/xplore/demosoap/UserTests.java
@@ -0,0 +1,37 @@
+package be.xplore.demosoap;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class UserTests {
+
+    private final User user1 = User.builder().firstName("Fons").lastName("Kwanten").role("Dev").id(1L).build();
+    private final User user2 = User.builder().firstName("Fons").lastName("Kwanten").role("Dev").id(1L).build();
+    private final User user3 = User.builder().firstName("Kwons").lastName("Fanten").role("Dev").id(1L).build();
+
+    @Test
+    public void testEqualsAndHashcode(){
+        assertThat(user1.equals(user2))
+                .as("Equals method not implemented correctly ")
+                .isTrue();
+        assertThat(user1.equals(user3))
+                .as("Equals method not implemented correctly ")
+                .isFalse();
+        assertThat(user1.equals(new ArrayList<>()))
+                .as("Equals method not implemented correctly ")
+                .isFalse();
+        assertThat(user1.hashCode() == user2.hashCode())
+                .as("Hashcode method not implemented correctly")
+                .isTrue();
+    }
+
+    @Test
+    public void testToString() {
+        assertThat(user1.toString())
+                .as("toString not implemented correctly")
+                .isEqualTo("User[id=1, firstName=Fons, lastName=Kwanten, role=Dev]");
+    }
+}
