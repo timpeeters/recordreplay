528a8a5c668e56a96e86772b77fad019ecbbbc56
Stijn Schack <stijn.schack@gmail.com>
2019-07-18 17:20:32 +0200

    Jetty integration test

--- recordreplay-jetty/src/test/java/be/xplore/recordreplayjetty/JettyIntegrationTestRest.java
+++ recordreplay-jetty/src/test/java/be/xplore/recordreplayjetty/JettyIntegrationTestRest.java
@@ -4,12 +4,15 @@ import be.xplore.fakes.model.Headers;
 import be.xplore.fakes.model.Request;
 import be.xplore.fakes.service.DefaultHttpClient;
 import be.xplore.fakes.service.HttpClient;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes =
@@ -24,15 +27,20 @@ public class JettyIntegrationTestRest {
     public void initJetty() {
         recordReplayJetty = new RecordReplayJetty(port + 1);
         client = new DefaultHttpClient();
+        recordReplayJetty.start();
+    }
+
+    @After
+    public void stopJetty() {
+        recordReplayJetty.stop();
     }
 
     @Test
-    public void contextLoads() {
-        recordReplayJetty.start();
+    public void sendRequest() {
         Request getUsersRequest = Request.Builder.get(String.format("http://localhost:%d%s", port+1, "/user/list"))
                 .headers(Headers.builder().applicationJson().build())
                 .build();
-        var Response = client.execute(getUsersRequest);
-        recordReplayJetty.stop();
+        var response = client.execute(getUsersRequest);
+        assertThat(response.getStatusCode()).isEqualTo(200);
     }
 }
