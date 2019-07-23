3a285643b437b769671df2d31264a11b189636dd
Stijn Schack <stijn.schack@gmail.com>
2019-07-23 16:22:46 +0200

    refactored servlets and use cases (issue #121, also solved #120 and #122) (PR#129)
    * usecase handling boilerplate in core #121
    
    * Servlet delegates UseCase to StubHandler in core module #121
    
    * rename AbstractHttpServlet to RecordReplayHttpServlet & PMD fixes  #121
    
    * Moved UseCases to seperate package & made them implement UseCase interface #121
    
    * removed dependency on 'okhttp-client' #120
    
    * removed redundant dependency
    
    * moved UseCase implementations to core
    
    * static getter and setter to anticipate use case configuration #121
    
    * fixed integration test #121
    
    * Made integration test compile (proper constructor use) #121
    
    * added override annotation
    
    * made RecordReplayHttpServlet constructor public #121

---
+++ fakes-core/src/main/java/be/xplore/recordreplay/usecase/UseCase.java
@@ -0,0 +1,10 @@
+package be.xplore.recordreplay.usecase;
+
+import be.xplore.fakes.model.Response;
+import be.xplore.fakes.model.Stub;
+
+import java.util.Optional;
+
+interface UseCase {
+    Optional<Response> execute(Stub stub);
+}
