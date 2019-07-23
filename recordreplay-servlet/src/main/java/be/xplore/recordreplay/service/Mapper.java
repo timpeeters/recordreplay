98e7c0692e9eeb6f012c86f0287c7d25099bc895
Stijn Schack <stijn.schack@gmail.com>
2019-07-23 13:35:32 +0200

    removed unused classes and interfaces #123

--- recordreplay-servlet/src/main/java/be/xplore/recordreplay/service/Mapper.java
+++
@@ -1,6 +0,0 @@
-package be.xplore.recordreplay.service;
-
-@FunctionalInterface
-public interface Mapper<R, P> {
-    void map(R r, P p);
-}
--- recordreplay-servlet/src/main/java/be/xplore/recordreplay/service/RequestMapper.java
+++
@@ -1,5 +0,0 @@
-package be.xplore.recordreplay.service;
-
-@FunctionalInterface
-public interface RequestMapper<R> extends Mapper<R, MappingRequest> {
-}
