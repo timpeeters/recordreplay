6c06bb123baf194ccc7829f3617288e7af933f7b
Stijn Schack <stijn.schack@gmail.com>
2019-07-23 17:15:49 +0200

    Added constructor with Configuration parameter

--- recordreplay-junit5/src/main/java/be/xplore/recordreplay/junit5/RecordReplayExtension.java
+++ recordreplay-junit5/src/main/java/be/xplore/recordreplay/junit5/RecordReplayExtension.java
@@ -1,18 +1,28 @@
 package be.xplore.recordreplay.junit5;
 
+import be.xplore.recordreplay.config.Configuration;
+import be.xplore.recordreplay.usecase.StubHandler;
+import be.xplore.recordreplayjetty.RecordReplayJetty;
 import org.junit.jupiter.api.extension.AfterAllCallback;
 import org.junit.jupiter.api.extension.BeforeAllCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
 
 public class RecordReplayExtension implements BeforeAllCallback, AfterAllCallback {
 
-    @Override
-    public void afterAll(ExtensionContext context) {
+    private final RecordReplayJetty recordReplayJetty;
 
+    public RecordReplayExtension(Configuration configuration) {
+        StubHandler.setCurrent(new StubHandler(configuration.useCase()));
+        recordReplayJetty = new RecordReplayJetty(configuration.port());
     }
 
     @Override
     public void beforeAll(ExtensionContext context) {
+        recordReplayJetty.start();
+    }
 
+    @Override
+    public void afterAll(ExtensionContext context) {
+        recordReplayJetty.stop();
     }
 }
