5038edfe1d26681aefeaa2d1dce74037aa761d6b
Stijn Schack <stijn.schack@gmail.com>
2019-07-23 14:53:52 +0200

    removed dependency on 'okhttp-client' #120

--- recordreplay-usecases/src/main/java/be/xplore/recordreplay/usecase/RecordUseCase.java
+++ recordreplay-usecases/src/main/java/be/xplore/recordreplay/usecase/RecordUseCase.java
@@ -2,8 +2,8 @@ package be.xplore.recordreplay.usecase;
 
 import be.xplore.fakes.model.Response;
 import be.xplore.fakes.model.Stub;
+import be.xplore.fakes.service.HttpClient;
 import be.xplore.fakes.service.Repository;
-import be.xplore.recordreplay.service.OkHttpClient;
 
 import java.util.Optional;
 
@@ -12,9 +12,9 @@ public class RecordUseCase implements UseCase {
     private final Repository repository;
     private final ForwardRequestUseCase forward;
 
-    public RecordUseCase(Repository repository) {
+    public RecordUseCase(Repository repository, HttpClient client) {
         this.repository = repository;
-        forward = new ForwardRequestUseCase(new OkHttpClient());
+        this.forward = new ForwardRequestUseCase(client);
     }
 
     public Optional<Response> execute(Stub stub) {
