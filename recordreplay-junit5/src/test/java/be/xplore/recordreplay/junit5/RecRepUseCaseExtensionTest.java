e0a91afdc1e54054629034b3e40279d6a62b22c0
Stijn Schack <stijn.schack@gmail.com>
2019-07-24 13:15:09 +0200

    use default matchers in test #72

--- recordreplay-junit5/src/test/java/be/xplore/recordreplay/junit5/RecRepUseCaseExtensionTest.java
+++ recordreplay-junit5/src/test/java/be/xplore/recordreplay/junit5/RecRepUseCaseExtensionTest.java
@@ -2,15 +2,8 @@ package be.xplore.recordreplay.junit5;
 
 import be.xplore.fakes.model.Response;
 import be.xplore.fakes.service.MemoryRepository;
-import be.xplore.fakes.service.RequestBodyMatcher;
-import be.xplore.fakes.service.RequestHeaderMatcher;
-import be.xplore.fakes.service.RequestMethodMatcher;
-import be.xplore.fakes.service.RequestParamMatcher;
-import be.xplore.fakes.service.RequestPathMatcher;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
-
-import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -18,14 +11,8 @@ class RecRepUseCaseExtensionTest extends ExtensionTestBase {
     private static final MemoryRepository REPO = new MemoryRepository();
     @RegisterExtension
     static RecordReplayExtension recordReplay = new RecordReplayExtension(CONFIG
-            .repository(REPO)
-            .matchers(List.of(
-                    new RequestMethodMatcher(),
-                    new RequestPathMatcher(),
-                    new RequestHeaderMatcher(),
-                    new RequestParamMatcher(),
-                    new RequestBodyMatcher()))
-    ).recordReplay();
+            .repository(REPO))
+            .recordReplay();
 
     @Test
     void testExtension() {
