a32a26c6214c779394262efd880f107ce9f1f415
Stijn Schack <stijn.schack@gmail.com>
2019-07-24 11:54:51 +0200

    Added record usecase test #72

--- recordreplay-junit5/src/test/java/be/xplore/recordreplay/junit5/ExtensionTestBase.java
+++ recordreplay-junit5/src/test/java/be/xplore/recordreplay/junit5/ExtensionTestBase.java
@@ -5,7 +5,6 @@ import be.xplore.fakes.model.Request;
 import be.xplore.fakes.model.Response;
 import be.xplore.fakes.service.DefaultHttpClient;
 import be.xplore.fakes.service.HttpClient;
-import be.xplore.recordreplay.config.Configuration;
 import be.xplore.recordreplay.config.RecordReplayConfig;
 import be.xplore.recordreplay.service.OkHttpClient;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,7 +13,7 @@ import org.springframework.boot.web.server.LocalServerPort;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes =
         be.xplore.demorest.DemoRestApplication.class)
 abstract class ExtensionTestBase {
-    static final Configuration CONFIG = new RecordReplayConfig().client(new OkHttpClient());
+    static final RecordReplayConfig CONFIG = new RecordReplayConfig().client(new OkHttpClient());
     private final HttpClient client = new DefaultHttpClient(CONFIG.host(), CONFIG.port());
 
     @LocalServerPort
