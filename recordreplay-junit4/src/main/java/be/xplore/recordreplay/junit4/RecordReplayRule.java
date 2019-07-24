5e19595d0c47a9a9decfde8a5a6464e4f4429448
Jeroen Timmers <jeroen.timmers@icloud.com>
2019-07-24 09:44:07 +0200

    "RecordReplayRule implements TestRule"

--- recordreplay-junit4/src/main/java/be/xplore/recordreplay/junit4/RecordReplayRule.java
+++ recordreplay-junit4/src/main/java/be/xplore/recordreplay/junit4/RecordReplayRule.java
@@ -1,4 +1,33 @@
 package be.xplore.recordreplay.junit4;
 
-public class RecordReplayRule {
+import be.xplore.fakes.config.Configuration;
+import org.junit.rules.TestRule;
+import org.junit.runner.Description;
+import org.junit.runners.model.Statement;
+
+public class RecordReplayRule implements TestRule {
+
+    public RecordReplayJetty recordreplay;
+
+    public RecordReplayRule(Configuration config) {
+
+    }
+
+    ;
+
+    @Override
+    public Statement apply(Statement base, Description description) {
+        return new Statement() {
+
+                try
+
+            {
+                base.evaluate();
+            }finally
+
+            {
+
+            }
+        };
+    }
 }
