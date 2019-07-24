84a161a2753dd48f8ac59ed78d38d2777b075599
Jeroen Timmers <jeroen.timmers@icloud.com>
2019-07-24 13:05:04 +0200

    "Added RecordReplay-rule test"

---
+++ recordreplay-junit4/src/test/java/be/xplore/recordreplay/junit4/RecordReplayRuleTest.java
@@ -0,0 +1,22 @@
+package be.xplore.recordreplay.junit4;
+
+import be.xplore.fakes.model.Response;
+import org.junit.ClassRule;
+import org.junit.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class RecordReplayRuleTest extends AbstractRuleTest {
+
+    @ClassRule
+    public static final RecordReplayRule RULE = new RecordReplayRule(CONFIG).recordReplay();
+
+    @Test
+    public void testRule() {
+        Response response = executeRequest();
+        assertThat(response.getStatusCode()).isEqualTo(200);
+        assertThat(REPO.find().size()).isEqualTo(1);
+        assertThat(response).isEqualTo(executeRequest());
+    }
+
+}
