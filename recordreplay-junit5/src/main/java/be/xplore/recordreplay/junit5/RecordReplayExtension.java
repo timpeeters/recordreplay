fb6a733f33571a1797da9f4705d5dcfa86268e49
Stijn Schack <stijn.schack@gmail.com>
2019-07-24 11:13:27 +0200

    extension configuration #72

--- recordreplay-junit5/src/main/java/be/xplore/recordreplay/junit5/RecordReplayExtension.java
+++ recordreplay-junit5/src/main/java/be/xplore/recordreplay/junit5/RecordReplayExtension.java
@@ -1,20 +1,27 @@
 package be.xplore.recordreplay.junit5;
 
 import be.xplore.recordreplay.config.Configuration;
+import be.xplore.recordreplay.usecase.ForwardRequestUseCase;
+import be.xplore.recordreplay.usecase.RecordUseCase;
 import be.xplore.recordreplay.usecase.StubHandler;
 import be.xplore.recordreplayjetty.RecordReplayJetty;
 import org.junit.jupiter.api.extension.AfterAllCallback;
 import org.junit.jupiter.api.extension.BeforeAllCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
 
+import static be.xplore.fakes.util.Assert.notNull;
+
 public class RecordReplayExtension implements BeforeAllCallback, AfterAllCallback {
 
     private final RecordReplayJetty recordReplayJetty;
+    private final Configuration configuration;
 
     public RecordReplayExtension(Configuration configuration) {
-        StubHandler.setCurrent(new StubHandler(configuration.useCase()));
+        this.configuration = notNull(configuration);
         recordReplayJetty = new RecordReplayJetty(configuration.port());
     }
+
+
 
     @Override
     public void beforeAll(ExtensionContext context) {
@@ -24,5 +31,15 @@ public class RecordReplayExtension implements BeforeAllCallback, AfterAllCallbac
     @Override
     public void afterAll(ExtensionContext context) {
         recordReplayJetty.stop();
+    }
+
+    public RecordReplayExtension record() {
+        StubHandler.setCurrent(new StubHandler(new RecordUseCase(configuration.repository(), configuration.client())));
+        return this;
+    }
+
+    public RecordReplayExtension forward() {
+        StubHandler.setCurrent(new StubHandler(new ForwardRequestUseCase(configuration.client())));
+        return this;
     }
 }
