9b4d9aaa444b0e0fa3fac94f48a1d5b7bb57057d
Stijn Schack <stijn.schack@gmail.com>
2019-07-25 16:31:00 +0200

    junit5 extension (issue #72) (#135)
    * junit 5 extention boilerplate (#72)
    
    * rename RecordReplayExtention to RecordReplayExtension
    
    * added basic Configuration interface (made UseCase public)
    
    * Added constructor with Configuration parameter
    
    * Created basic configuration implementation #115
    
    * added test dependencies #72
    
    * Wrote test for junit5 extension
    
    * extension configuration #72
    
    * default listen address #115
    
    * setting UseCase now happens outside of RecordReplayJetty #115
    
    * fix PMD and Checkstyle issues #72
    
    * refactor test so multiple tests can be made easily #72
    
    * Added record usecase test #72
    
    * Replay UseCase test #72
    
    * Added RecordReplay UseCase test #72
    
    * fixed checkstyle issue
    
    * made ExtensionTestBase not abstract
    
    * added default matchers to config #115
    
    * added default repository to RecordReplayConfig #115
    
    * use default matchers in test #72
    
    * fixed junit5 extension to match junit4 rule #72
    
    * use junit5 platform when running tests in extension module #72

---
+++ recordreplay-junit5/src/test/java/be/xplore/recordreplay/junit5/ExtensionTestBase.java
@@ -0,0 +1,27 @@
+package be.xplore.recordreplay.junit5;
+
+import be.xplore.fakes.model.Headers;
+import be.xplore.fakes.model.Request;
+import be.xplore.fakes.model.Response;
+import be.xplore.fakes.service.DefaultHttpClient;
+import be.xplore.fakes.service.HttpClient;
+import be.xplore.recordreplay.config.RecordReplayConfig;
+import be.xplore.recordreplay.service.OkHttpClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes =
+        be.xplore.demorest.DemoRestApplication.class)
+class ExtensionTestBase {
+    static final RecordReplayConfig CONFIG = new RecordReplayConfig().client(OkHttpClient.noProxy());
+    private final HttpClient client = new DefaultHttpClient();
+
+    @LocalServerPort
+    private int port;
+
+    Response executeRequest() {
+        return client.execute(Request.Builder
+                .get(String.format("http://localhost:%d/user/list", port))
+                .headers(Headers.builder().applicationJson().build()).build());
+    }
+}
---
+++ recordreplay-junit5/src/test/java/be/xplore/recordreplay/junit5/RecRepUseCaseExtensionTest.java
@@ -0,0 +1,24 @@
+package be.xplore.recordreplay.junit5;
+
+import be.xplore.fakes.model.Response;
+import be.xplore.fakes.service.MemoryRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class RecRepUseCaseExtensionTest extends ExtensionTestBase {
+    private static final MemoryRepository REPO = new MemoryRepository();
+    @RegisterExtension
+    static RecordReplayExtension recordReplay = new RecordReplayExtension(CONFIG
+            .repository(REPO))
+            .recordReplay();
+
+    @Test
+    void testExtension() {
+        Response response = executeRequest();
+        assertThat(response.getStatusCode()).isEqualTo(200);
+        assertThat(REPO.find().size()).isEqualTo(1);
+        assertThat(response).isEqualTo(executeRequest());
+    }
+}
