cce07c7374dd22510b95589f0e49f2b1cbf28b59
Stijn Schack <stijn.schack@gmail.com>
2019-07-29 10:02:41 +0200

    implement starter in junit 4 rule #152

--- recordreplay-junit4/src/main/java/be/xplore/recordreplay/junit4/RecordReplayRule.java
+++ recordreplay-junit4/src/main/java/be/xplore/recordreplay/junit4/RecordReplayRule.java
@@ -2,23 +2,16 @@ package be.xplore.recordreplay.junit4;
 
 import be.xplore.recordreplay.RecordReplay;
 import be.xplore.recordreplay.config.Configuration;
-import be.xplore.recordreplay.jetty.RecordReplayJetty;
-import be.xplore.recordreplay.proxy.ProxyManager;
-import be.xplore.recordreplay.usecase.StubHandler;
-import be.xplore.recordreplay.usecase.UseCase;
 import org.junit.rules.TestRule;
 import org.junit.runner.Description;
 import org.junit.runners.model.Statement;
 
-public class RecordReplayRule extends RecordReplay<RecordReplayRule> implements TestRule {
+public class RecordReplayRule implements TestRule {
 
-    private RecordReplayJetty recordReplay;
-    private final ProxyManager proxyManager;
-
+    private final RecordReplay recordReplay;
 
     public RecordReplayRule(Configuration configuration) {
-        super(configuration);
-        proxyManager = new ProxyManager();
+        recordReplay = new RecordReplay(configuration);
     }
 
     @Override
@@ -26,35 +19,33 @@ public class RecordReplayRule extends RecordReplay<RecordReplayRule> implements 
         return new Statement() {
             @Override
             public void evaluate() throws Throwable {
-                start();
+                recordReplay.start();
                 try {
                     base.evaluate();
                 } finally {
-                    stop();
+                    recordReplay.stop();
                 }
             }
         };
     }
 
-    private void stop() {
-        if (this.recordReplay != null) {
-            this.recordReplay.stop();
-        }
-        proxyManager.deActivate();
+    public RecordReplayRule replay() {
+        recordReplay.replay();
+        return this;
     }
 
-    private void start() {
-        stop();
-        if (this.recordReplay != null) {
-            this.recordReplay.start();
-            proxyManager.activate(recordReplay.getHost(), recordReplay.getPort());
-        }
+    public RecordReplayRule forward() {
+        recordReplay.forward();
+        return this;
     }
 
-    @Override
-    protected void createRecordReplay(UseCase useCase) {
-        stop();
-        this.recordReplay = new RecordReplayJetty(getConfig().port(), new StubHandler(useCase));
-        start();
+    public RecordReplayRule record() {
+        recordReplay.record();
+        return this;
+    }
+
+    public RecordReplayRule recordReplay() {
+        recordReplay.record();
+        return this;
     }
 }
