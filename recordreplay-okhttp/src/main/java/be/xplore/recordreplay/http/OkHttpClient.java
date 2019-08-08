1dde8ef99d482fc4288aae97695d49e89d293d66
Jeroen Timmers <jeroen.timmers@icloud.com>
2019-08-08 11:30:10 +0200

    Refactored constructor

--- recordreplay-okhttp/src/main/java/be/xplore/recordreplay/http/OkHttpClient.java
+++ recordreplay-okhttp/src/main/java/be/xplore/recordreplay/http/OkHttpClient.java
@@ -19,7 +19,7 @@ public class OkHttpClient implements HttpClient {
     private okhttp3.OkHttpClient client;
 
     public OkHttpClient() {
-        proxy(Proxy.NO_PROXY);
+        client = new okhttp3.OkHttpClient.Builder().proxy(Proxy.NO_PROXY).build();
     }
 
     @Override
